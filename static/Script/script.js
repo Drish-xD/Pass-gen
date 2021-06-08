becc3132b246046f55ce28abffa9dc23f1e886c2
Drish <drish.xd@gmail.com>
2021-06-08 18:30:38 +0530

    Update Bug Fixes

--- static/Script/script.js
+++ static/Script/script.js
@@ -1,25 +1,13 @@
-// slider value
-
-var elem = document.querySelector('input[type="range"]');
-
-var rangeValue = function () {
-  var newValue = elem.value;
-  var target = document.querySelector(".value");
-  target.innerHTML = newValue;
-};
-
-elem.addEventListener("input", rangeValue);
-
 // copy button
 
-$(document).on("click", ".copy-btn", function () {
+function copypassword() {
   var range = document.createRange();
   range.selectNode(document.getElementById("output"));
   window.getSelection().removeAllRanges(); // clear current selection
   window.getSelection().addRange(range); // to select text
   document.execCommand("copy");
-  window.getSelection().removeAllRanges(); // to deselect
-});
+  window.getSelection().removeAllRanges();// to deselect
+}
 
 // reload button
 
